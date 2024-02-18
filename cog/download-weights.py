8df010d18f5a79dccf552de007ae494df5e439c5
Terentius <tgohblio@gmail.com>
2024-02-18 22:48:51 +0800

    revert removing extract option

--- cog/download-weights.py
+++ cog/download-weights.py
@@ -33,7 +33,7 @@ def download_weights(url, dest):
     start = time.time()
     print("downloading url: ", url)
     print("downloading to: ", dest)
-    subprocess.check_call(["pget", url, dest], close_fds=False)
+    subprocess.check_call(["pget", "-x", url, dest], close_fds=False)
     print("downloading took: ", time.time() - start)
 
 if not os.path.exists(MODELS_CACHE):
